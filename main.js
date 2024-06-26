9a08cc1207f80a6251f4a0568805781d58daf9b6
Xabibulloh-J <xjorayev378@gmail.com>
2024-06-26 23:05:05 +0400

    Fixed

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var dollarExchangeRate = 12.600;
-var euroExchangeRate = 13.400;
+var dollarExchangeRate = 12600;
+var euroExchangeRate = 13400;
 
 var travelTicketPrice = 500;
 var hotelPrice = 250;
@@ -8,7 +8,7 @@ var museumPrice = 120;
 var userMoney = Number(prompt("Sayohat uchun sarflaydigan pulingizni kiriting:"));
 
 
-if((dollarExchangeRate.toFixed(3)*(travelTicketPrice+hotelPrice)+euroExchangeRate.toFixed(3)*museumPrice) <= userMoney){
+if((dollarExchangeRate*(travelTicketPrice+hotelPrice)+euroExchangeRate*museumPrice) <= userMoney){
    console.log("Sayohat uchun pulingiz yetadi, oq yo'l!");
 } else {
    console.log("Sayohat uchun pulingiz yetmaydi, biroz pul yeg'ing!");
